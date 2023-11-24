bfa47b0814acbaa5df4e0b8a24336ac21da639f9
Scott12141 <12141@coderacademy.edu.au>
2023-11-24 13:18:01 +1100

    set up cloud DB with enviroment variables

--- src/mongooseConnector.js
+++ src/mongooseConnector.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 async function databaseConnect(){
     try {
         // DB connection can take some time, so we have an await for this purpose
-        await mongoose.connect('mongodb://localhost:27017/tmdbDB');
+        await mongoose.connect(process.env.DB_URI);
         console.log("Database connected");
     } catch (error) {
         console.warn(`databaseConnect failed to connect to DB:\n${JSON.stringify(error)}`);
