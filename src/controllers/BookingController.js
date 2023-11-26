9761ed6f84dfbe2668e5a40493d45bb80c7b0994
Scott12141 <12141@coderacademy.edu.au>
2023-11-26 19:20:35 +1100

    Created Bookings routes

--- src/controllers/BookingController.js
+++ src/controllers/BookingController.js
@@ -10,4 +10,30 @@ router.get("/all", async (request, response) => {
     });
 });
 
+router.get("/multiple/date/findBookingsByDate", async (request, response) => {
+    let result = null
+
+    response.json({
+        bookings: result
+    });
+});
+
+//POST new booking to DB
+router.post("/", async (request, response) => {
+    let result = await Booking.create(request.body);
+
+    response.json({
+        bookings: result
+    });
+});
+
+router.delete("/:id", async (request, response) => {
+    let result = null
+
+    response.json({
+        bookings: result
+    });
+});
+
+
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ const { app } = require('./server');
 
 app.listen(3500, async () => {
     await databaseConnect();
-    console.log("Server is running!");
+    console.log("Server is running on port:3500");
 });
