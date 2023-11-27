44565158ab87df320a4881516b2f722ec89bcd3d
Scott12141 <12141@coderacademy.edu.au>
2023-11-27 17:50:54 +1100

    Bookings CRUD

--- src/controllers/BookingController.js
+++ src/controllers/BookingController.js
@@ -2,36 +2,38 @@ const express = require('express');
 const { Booking } = require('../models/BookingModel');
 const router = express.Router();
 
+//GET all bookings from DB
 router.get("/all", async (request, response) => {
-    let result = await Booking.find({});
+    let result = await Booking.find({}).catch(error => {return error});
 
     response.json({
         bookings: result
     });
 });
 
-router.get("/multiple/date/findBookingsByDate", async (request, response) => {
-    let result = null
+// router.get("/multiple/date/findBookingsByDate", async (request, response) => {
+//     let result = null
 
-    response.json({
-        bookings: result
-    });
-});
+//     response.json({
+//         bookings: result
+//     });
+// });
 
 //POST new booking to DB
 router.post("/", async (request, response) => {
-    let result = await Booking.create(request.body);
+    let result = await Booking.create(request.body).catch(error => {return error});
 
     response.json({
         bookings: result
     });
 });
 
+//Delete single booking from DB
 router.delete("/:id", async (request, response) => {
-    let result = null
+    let result = await Booking.findByIdAndDelete(request.params.id);
 
     response.json({
-        bookings: result
+        deletedBooking: result
     });
 });
 
--- src/mongooseConnector.js
+++ src/mongooseConnector.js
@@ -5,7 +5,7 @@ async function databaseConnect(){
     try {
         console.log("Connecting to: mongodb+srv:@cloudtmdbdb.8rfhvke.mongodb.net/?retryWrites=true&w=majority")
         await mongoose.connect(process.env.DB_URI);
-        console.log("Database connected");
+        console.log("Database connected!");
     } catch (error) {
         console.warn(`databaseConnect failed to connect to DB:\n${JSON.stringify(error)}`);
     }
