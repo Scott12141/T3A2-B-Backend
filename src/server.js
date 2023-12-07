a91ef635e45ab880eb8fa48dd42e4522aaba78a6
Scott12141 <12141@coderacademy.edu.au>
2023-12-07 17:49:01 +1100

    Added cors setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+const cors = require('cors');
+const corsOptions = {
+    origin: ["http://localhost:3500", "https://someDeployedWebsite.com"],
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.get("/", (request, response) => {
